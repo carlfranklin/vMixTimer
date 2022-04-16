d6e2e76218c9c11c7ae26c7829b9814deff07237
Carl Franklin <carl@franklins.net>
2022-04-16 11:28:14 -0400

    Fixed a typo. Added some comments

--- vMixTimer/Program.cs
+++ vMixTimer/Program.cs
@@ -1,4 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+This little .NET Console app works with vMix (https://www.vmix.com/) 
+to start streaming at a specified time, cut to a video or other input,
+and stop streaming after a certain amount of time has elapsed.
+
+You must set your stream settings and get your input ready before
+starting.
+ */
+
 Console.WriteLine("vMix Timer by Carl Franklin");
 Console.WriteLine("Kicks off a vMix event at a specified time.");
 Console.WriteLine("https://github.com/carlfranklin/vmixtimer");
@@ -7,9 +15,9 @@ Console.WriteLine("");
 Console.WriteLine($"The current time is {DateTime.Now.ToShortTimeString()}");
 Console.WriteLine("Enter start time in 24-hour format (HH:MM:SS):");
 
+// ask for the start time, checking for valid input
 var startTimeString = "";
 DateTime startTime = DateTime.Now;
-
 bool valid = false;
 while (!valid)
 {
@@ -24,10 +32,10 @@ while (!valid)
         Console.WriteLine(ex.Message);
     }
 }
-
 Console.WriteLine($"Streaming will start at {startTime.ToLongTimeString()}");
-Console.WriteLine("Enter desired duration of the stream in HH:MM::SS format:");
 
+// ask for the duration
+Console.WriteLine("Enter desired duration of the stream in HH:MM::SS format:");
 valid = false;
 var durationString = "";
 TimeSpan duration = TimeSpan.FromSeconds(0);
@@ -47,15 +55,19 @@ while (!valid)
     }
 }
 
-
+// show the user when streaming will start and stop.
 Console.WriteLine($"Streaming will start at {startTime.ToLongTimeString()}");
 Console.WriteLine($"Streaming will end at {endTime.ToLongTimeString()}");
 
+// tell the user to press ENTER when ready
 Console.WriteLine("Cue up the input in the orange frame on the left, and press ENTER when ready to start the countdown.");
 Console.ReadLine();
+
+// get the current line number in the console
 var pos = Console.GetCursorPosition();
 var top = pos.Top;
 
+// wait for the start time, updating the display every second
 while(true)
 {
     if (DateTime.Now >= startTime)
@@ -70,6 +82,7 @@ while(true)
 }
 Console.WriteLine();
 
+// let's go!
 string baseUrl = "http://127.0.0.1:8088/API/";
 var startStreamingUrl = baseUrl + "?Function=StartStreaming";
 var stopStreamingUrl = baseUrl + "?Function=StopStreaming";
@@ -77,6 +90,8 @@ var cutUrl = baseUrl + "?Function=Cut";
 
 // start streaming
 SendCommand(startStreamingUrl).GetAwaiter().GetResult();
+Console.WriteLine("Streaming");
+
 // wait 3 seconds
 Task.Delay(3000).GetAwaiter().GetResult();
 // cut to input
@@ -84,10 +99,11 @@ SendCommand(cutUrl).GetAwaiter().GetResult();
 // wait 3 seconds
 Task.Delay(3000).GetAwaiter().GetResult();
 
-Console.WriteLine("Streaming");
+// get current line again
 pos = Console.GetCursorPosition();
 top = pos.Top;
 
+// wait until the time is up, updating the display every second
 while (true)
 {
     if (DateTime.Now >= endTime)
@@ -98,15 +114,15 @@ while (true)
     // display the countdown
     var timeleft = endTime - DateTime.Now;
     Console.SetCursorPosition(0, top);
-    Console.Write($"Stream will start in {Convert.ToInt32(Math.Abs(timeleft.TotalSeconds))} seconds    ");
+    Console.Write($"Stream will stop in {Convert.ToInt32(Math.Abs(timeleft.TotalSeconds))} seconds    ");
 }
 Console.WriteLine();
 
 // stop streaming
 SendCommand(stopStreamingUrl).GetAwaiter().GetResult();
-// wait one second
-Task.Delay(1000).GetAwaiter().GetResult();
 Console.WriteLine("Streaming has stopped.");
+
+// and we're done!
 
 return;
 
